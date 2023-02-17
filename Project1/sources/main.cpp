13860be1951a16074d5345ea27c1153036759894
DhahriHaythem <6182830@bdeb.qc.ca>
2023-02-16 21:02:26 -0500

    Clean p1 + p 2
    Clean up project1
    Added project2

--- Project1/sources/main.cpp
+++ Project1/sources/main.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<sstream>
 
-#define GLEW_STATIC
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
---
+++ Project2/sources/main.cpp
@@ -0,0 +1,193 @@
+#include<iostream>
+#include<sstream>
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+const char* APP_TITLE = "Introduction to Modern OpenGL - Hello Triangle";
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+GLFWwindow* pWindow = NULL;
+
+const GLchar* vertexShaderSrc =
+"#version 330 core\n"
+"layout (location = 0) in vec3 pos;"
+"void main()"
+"{"
+"	gl_Position = vec4(pos.x,pos.y,pos.z,1);"
+"}";
+
+const GLchar* fragmentShaderSrc =
+"#version 330 core\n"
+"out vec4 frag_color;"
+"void main()"
+"{"
+"	frag_color = vec4(0.35f,0.96f,0.3f,1.0f);"
+"}";
+
+
+void glfw_Onkey(GLFWwindow* window, int key, int scancode, int action, int mode);
+void showFPS(GLFWwindow* window);
+bool InitOpenGL();
+
+int main()
+{
+	if (!InitOpenGL())
+	{
+		std::cerr << "GLFW initialization failed " << std::endl;
+		return -1;
+	}
+
+	GLfloat vertices[] = {
+		0.0f,0.5f,0.0f,
+		0.5f,-0.5f,0.0f,
+		-0.5f,-0.5f,0.0f
+	};
+
+	GLuint vbo, vao;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(0);
+
+	//vertex shader
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertexShaderSrc, NULL);
+	glCompileShader(vs);
+
+	GLint result;
+	GLchar infolog[512];
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vs, sizeof(infolog), NULL, infolog);
+		std::cout << "Error! vertex shader failed to compile." << infolog << std::endl;
+	}
+
+	//fragment shader
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragmentShaderSrc, NULL);
+	glCompileShader(fs);
+
+	glGetShaderiv(fs, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fs, sizeof(infolog), NULL, infolog);
+		std::cout << "Error! fragment shader failed to compile." << infolog << std::endl;
+	}
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vs);
+	glAttachShader(shaderProgram, fs);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &result);
+	if (!result)
+	{
+		glGetProgramInfoLog(fs, sizeof(infolog), NULL, infolog);
+		std::cout << "Error! shader Program linker failed to compile." << infolog << std::endl;
+	}
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	while (!glfwWindowShouldClose(pWindow))
+	{
+		showFPS(pWindow);
+
+		glfwPollEvents();
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
+		glfwSwapBuffers(pWindow);
+	}
+
+	glDeleteProgram(shaderProgram);
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
+
+	glfwTerminate();
+	return 0;
+}
+
+bool InitOpenGL()
+{
+	if (!glfwInit())
+	{
+		std::cerr << "GLFW initialization failed" << std::endl;
+		return false;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	pWindow = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, APP_TITLE, NULL, NULL);
+
+	if (pWindow == NULL)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return false;
+	}
+
+	glfwMakeContextCurrent(pWindow);
+	glfwSetKeyCallback(pWindow, glfw_Onkey);
+
+	GLboolean glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "GLEW initialization failed" << std::endl;
+		return false;
+	}
+	glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+
+
+	return true;
+}
+
+void glfw_Onkey(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
+
+void showFPS(GLFWwindow* window)
+{
+	static double previousSeconds = 0.0;
+	static int frameCount = 0;
+	double elapsedSeconds;
+	double currentSeconds = glfwGetTime();
+
+	elapsedSeconds = currentSeconds - previousSeconds;
+
+	if (elapsedSeconds > 0.25)//update text 4 times per second
+	{
+		previousSeconds = currentSeconds;
+		double fps = (double)frameCount / elapsedSeconds;
+		double msPerFrame = 1000.0 / fps;
+		std::ostringstream outs;
+		outs.precision(3);
+		outs << std::fixed
+			<< APP_TITLE << "  "
+			<< "FPS: " << fps << " "
+			<< "Frame Time; " << msPerFrame << " (ms)";
+		glfwSetWindowTitle(window, outs.str().c_str());
+		frameCount = 0;
+	}
+	frameCount++;
+}
